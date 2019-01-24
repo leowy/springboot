ecbb71d918adfdb292363c98d29f9602dcb33d48
leowyzhuang@gmail.com <leowy>
2019-01-24 08:58:25 +0800

    add Readme.md

---
+++ springboot-mybatis/Readme.md
@@ -0,0 +1,4 @@
+# 注意
+
+@Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")  
+单条数据插入数据库返回自动生成的id，但是批量插入不支持
--- springboot-mybatis/src/main/java/com/leo/mapper/UserMapper.java
+++ springboot-mybatis/src/main/java/com/leo/mapper/UserMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.DeleteProvider;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.junit.runners.Parameterized.Parameters;
 
 import com.leo.entity.User;
 
